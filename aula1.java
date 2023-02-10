bbcd18354497ebb7442836a575f29be255bd689d
ThiagoDaSilvaSoares <124941089+ThiagoDaSilvaSoares@users.noreply.github.com>
2023-02-10 00:58:24 +0000

    Please enter the commit message for your changes. Lines starting

--- aula1.java
+++ aula1.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class aula1 {
+    public static void main(String[] args) {
+        System.out.println("Digite aqui seu nome");
+        String nome;
+        int contador = 0;
+        while (contador < 100) { 
+            nome = "Kaique"; 
+            System.out.println("Nome kkkk" + nome);
+            contador++;
+        }
+    }
+}
